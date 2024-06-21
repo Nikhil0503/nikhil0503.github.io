308e0171126a8d761ea2c30146cd0a893a96252e
Nikhil Saireddy Munagala <nikhilsai.munagala@gmail.com>
2024-06-21 09:34:27 -0400

    Reorganized the website.

--- sendEmail.js
+++ sendEmail.js
@@ -3,40 +3,68 @@ var submitBtn = document.querySelector(".btn.btn-primary.submit");
 
 //Get the text fields
 var nameInput = document.querySelector(".name");
+var dataList1 = document.querySelector(".previous1-options");
 var subjectInput = document.querySelector(".subject");
+var dataList2 = document.querySelector(".previous2-options");
 var emailAddressInput = document.querySelector(".email-address");
+var dataList3 = document.querySelector(".previous3-options");
 var emailMessageInput = document.querySelector("textarea");
 
-var count = 0; //Count for the number of times you enter empty fields
+//Maintain a list of 
+
+//Maintain a Hashmap for easy access for Data Lists based on the name of the datalist.
+let nameDataList = {};
+nameDataList[nameInput] = dataList1;
+nameDataList[subjectInput] = dataList2;
+nameDataList[emailAddressInput] = dataList3;
+
+var invalid = false; //No invalid message being generated in the beginning. 
 
 var ivmsg = null; //This is the global variable used for the invalid msg.
+
+function loadOptions(tfInputField) {
+    const storedOptions = JSON.parse(localStorage.getItem(tfInputField.className)) || [];
+    nameDataList[tfInputField].innerHTML = '';
+    storedOptions.forEach(option => {
+        const optionElement = document.createElement('option');
+        optionElement.value = option;
+        nameDataList[tfInputField].appendChild(optionElement);
+    });
+}
+
+function saveOption(value, dataListName) {
+    const storedOptions = JSON.parse(localStorage.getItem(dataListName)) || [];
+    if (!storedOptions.includes(value)) {
+        storedOptions.push(value);
+        localStorage.setItem(dataListName, JSON.stringify(storedOptions));
+    }
+}
 
 // Set the event listener for the submit button
 submitBtn.addEventListener('click', function(e){
     e.preventDefault();
     // Get the values from the input fields
-    var name = nameInput.value;
-    var subject = subjectInput.value;
-    var emailAddress = emailAddressInput.value;
-    var emailMessage = emailMessageInput.value;
-
+    var { name, subject, emailAddress, emailMessage } = getInputVals();
+    
     // Check if any of the fields are empty
     if (name == '' ||  subject == '' || emailAddress == '' || emailMessage == '') {
         //Create the text field please fill in all fields below in red.
         //Make the paragraph first.
-        if (count == 0){
+        if (!invalid){
             ivmsg = generateIVMessage();
+            invalid = true;
         }
-        count++;
         return; // Exit the function without sending the email
     } else {
         //Clear the invalid message that was generated, don't keep it there!
-        if (count >= 1){
+        if (invalid){
             var form = document.querySelector(".contact-form");
             form.removeChild(ivmsg);
             ivmsg = null;
             count = 0;
+            invalid = false;
         }
+        
         // Send the email using an email service (e.g., Email.js)
         sendingEmail(name, emailAddress, subject, emailMessage);
         nameInput.value = '';
@@ -46,14 +74,20 @@ submitBtn.addEventListener('click', function(e){
     }
 });
 
+function getInputVals() {
+    var name = nameInput.value;
+    var subject = subjectInput.value;
+    var emailAddress = emailAddressInput.value;
+    var emailMessage = emailMessageInput.value;
+    return { name, subject, emailAddress, emailMessage };
+}
+
 function generateIVMessage() {
     var para = document.createElement("p");
-    //Add the following information in the paragraph.
     para.innerHTML = "Please fill in all of the information above!";
-    //Make the paragraph red then.
     para.style.color = "red";
     para.style.fontFamily = 'Times New Roman, Times, serif';
-    //Then get the parent element where the text will be placed in and append it there.
+    //Get the parent element where the text will be placed in and append it there.
     var form = document.querySelector(".contact-form");
     form.appendChild(para);
     return para
@@ -65,7 +99,7 @@ function sendingEmail(name, emailAddress, subject, msg){
     emailjs.send(window.env.SERVICE_ID, window.env.TEMPLATE_ID, params).then(
         res => {
             console.log(res);
-            alert("Your message has been successfully sent!")
+            alert("Your message has been successfully sent!");
         }
     ).catch((err) => console.log(err));
 }
